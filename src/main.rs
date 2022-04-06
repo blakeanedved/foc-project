5ba854421cdc79de8c9102b461ff9a0ff43baf11
Blake Nedved <blakeanedved@gmail.com>
2022-04-06 06:15:10 -0500

    basic expression parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,133 @@
+extern crate nom;
+
+mod shunting_yard;
+
+use shunting_yard::shunting_yard;
+
+use nom::{
+  IResult,
+  error::ParseError,
+  branch::alt,
+  bytes::complete::tag,
+  combinator::{map, recognize, success},
+  character::complete::{multispace0, i32, alpha1, alphanumeric1},
+  sequence::{delimited, pair, preceded}, multi::{many0, many0_count}};
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ExprToken {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    LParen,
+    RParen,
+    Number(i32),
+    Ident(String),
+    Call { name: String, args: Vec<Vec<ExprToken>> }
+}
+
+#[derive(Debug)]
+pub enum Expr {
+    Add(Box<Expr>, Box<Expr>),
+    Sub(Box<Expr>, Box<Expr>),
+    Mul(Box<Expr>, Box<Expr>),
+    Div(Box<Expr>, Box<Expr>),
+    Number(i32),
+    Ident(String),
+    Call { name: String, args: Vec<Box<Expr>> }
+}
+
+fn ws<'a, F: 'a, O, E: ParseError<&'a str>>(inner: F) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
+  where
+  F: Fn(&'a str) -> IResult<&'a str, O, E>,
+{
+    delimited(
+        multispace0,
+        inner,
+        multispace0
+    )
+}
+
+fn ident(input: &str) -> IResult<&str, &str> {
+    recognize(
+        pair(
+            alt((alpha1, tag("_"))),
+            many0_count(alt((alphanumeric1, tag("_"))))
+        )
+    )(input)
+}
+
+fn unary(input: &str) -> IResult<&str, ExprToken> {
+    alt((
+        map(i32, |v| ExprToken::Number(v)),
+        map(
+            pair(ident, delimited(ws(tag("(")), expr_list, ws(tag(")")))),
+            |(i, e)| ExprToken::Call{ name: String::from(i), args: e }
+        ),
+        map(ident, |s| ExprToken::Ident(String::from(s))),
+    ))(input)
+}
+
+fn term(input: &str) -> IResult<&str, Vec<ExprToken>> {
+    alt((
+        map(
+            delimited(ws(tag("(")), expr, ws(tag(")"))),
+            |mut e: Vec<ExprToken>| {
+                e.insert(0, ExprToken::LParen);
+                e.push(ExprToken::RParen);
+                e
+            }
+        ),
+        map(unary, |u| vec![u])
+    ))(input)
+}
+
+fn op(input: &str) -> IResult<&str, ExprToken> {
+    map(
+        alt((tag("+"), tag("-"), tag("*"), tag("/"))),
+        |op| {
+            use ExprToken::*;
+            match op {
+                "+" => Add,
+                "-" => Sub,
+                "*" => Mul,
+                "/" => Div,
+                _ => unreachable!()
+            }
+        }
+    )(input)
+}
+
+fn expr(input: &str) -> IResult<&str, Vec<ExprToken>> {
+    map(
+        pair(ws(term), many0(pair(ws(op), ws(term)))),
+        |(mut e, l)| {
+            for (op, e2) in l {
+                e.push(op);
+                e.extend(e2.into_iter());
+            }
+            e
+        }
+    )(input)
+}
+
+fn expr_list(input: &str) -> IResult<&str, Vec<Vec<ExprToken>>> {
+    alt((
+        map(
+            pair(expr, many0(preceded(ws(tag(",")), expr))),
+            |(e, el)| {
+                let mut v = vec![e];
+                for e in el {
+                    v.push(e);
+                }
+                v
+            }
+        ),
+        success(vec![])
+    ))(input)
+}
+
 fn main() {
-    println!("Hello, world!");
+  println!("{:?}", shunting_yard(&mut expr("f(x*2, y+(5/3))").unwrap().1));
+  println!("{:?}", shunting_yard(&mut expr("5 * f ( x )").unwrap().1));
 }
---
+++ src/shunting_yard.rs
@@ -0,0 +1,90 @@
+use crate::{Expr, ExprToken};
+
+fn precedence(op: &ExprToken) -> i16 {
+    use ExprToken::*;
+    match op {
+        Add => 1,
+        Sub => 1,
+        Mul => 2,
+        Div => 2,
+        LParen => 0,
+        RParen => -1,
+        _ => unreachable!()
+    }
+}
+
+fn is_op(e: &ExprToken) -> bool {
+    use ExprToken::*;
+    match e {
+        Add | Sub | Mul | Div | LParen | RParen => true,
+        _ => false,
+    }
+}
+
+pub fn shunting_yard(tokens: &mut Vec<ExprToken>) -> Box<Expr> {
+    let mut rpn = Vec::with_capacity(tokens.len());
+    let mut op_stack: Vec<(i16, &mut ExprToken)> = Vec::new();
+
+    for token in tokens.into_iter() {
+        if is_op(token) {
+            let prec = precedence(token);
+            
+            while !op_stack.is_empty() {
+                if *token == ExprToken::LParen {
+                    break;
+                } else if prec < op_stack.last().unwrap().0 {
+                    let op = op_stack.pop().unwrap();
+
+                    if *op.1 == ExprToken::LParen && *token == ExprToken::RParen {
+                        break;
+                    } else {
+                        rpn.push(op.1)
+                    }
+                } /* TODO equal precedence left associative popping */ else {
+                    break;
+                }
+            }
+
+            if *token != ExprToken::RParen {
+                op_stack.push((prec, token));
+            }
+        } else {
+            rpn.push(token);
+        }
+    }
+
+    while !op_stack.is_empty() {
+        let op = op_stack.pop().unwrap();
+
+        if *op.1 != ExprToken::LParen {
+            rpn.push(op.1)
+        }
+    }
+
+    let mut expr_trees = vec![];
+
+    for token in rpn.into_iter() {
+        use ExprToken::*;
+        if is_op(token) {
+            let rhs = expr_trees.pop().unwrap();
+            let lhs = expr_trees.pop().unwrap();
+            let new_expr = match token {
+                Add => Expr::Add(lhs, rhs),
+                Sub => Expr::Sub(lhs, rhs),
+                Mul => Expr::Mul(lhs, rhs),
+                Div => Expr::Div(lhs, rhs),
+                _ => unreachable!()
+            };
+            expr_trees.push(Box::new(new_expr));
+        } else {
+            expr_trees.push(Box::new(match token {
+                Number(v) => Expr::Number(*v),
+                Ident(s) => Expr::Ident(s.clone()),
+                Call { name, args } => Expr::Call { name: name.clone(), args: args.into_iter().map(shunting_yard).collect() },
+                _ =>  unreachable!()
+            }));
+        }
+    }
+
+    expr_trees.swap_remove(0)
+}
