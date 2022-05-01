3d496e5a730a94246e153736b7b2fdc878bf720a
Blake Nedved <blakeanedved@gmail.com>
2022-05-01 17:24:42 -0500

    fixed function naming

--- src/utils.rs
+++ src/utils.rs
@@ -3,10 +3,10 @@ use rand::{thread_rng, Rng};
 
 pub fn generate_function_name(name: impl AsRef<str>) -> String {
     format!(
-        "{}_{}",
+        "f{}_{}",
         thread_rng()
             .sample_iter(&Alphanumeric)
-            .take(16)
+            .take(15)
             .map(char::from)
             .collect::<String>(),
         name.as_ref()
